fcf3ad2a73184491e22f502b46b4f4ec5c50d99d
jb0001994 <jb0001994@aol.com>
2015-04-05 17:42:42 -0700

    binary interpretation complete

--- BigInt/Intpossible.java
+++ BigInt/Intpossible.java
@@ -1,3 +1,4 @@
+import java.lang.*;
 import java.lang.Override;
 import java.lang.String;
 import java.lang.StringBuilder;
@@ -23,7 +24,7 @@ public class Intpossible {
         if(args.length == 0){
             Intpossible bigInt = new Intpossible();
         }else {
-            System.out.println(args[0]);
+//            System.out.println(args[0]);
             Intpossible bigInt = new Intpossible(args[0]);
             System.out.println("digits: " + bigInt.digits.toString());
             System.out.println(bigInt);
@@ -63,8 +64,8 @@ public class Intpossible {
                 newValue.insert(0, divideBy2(s.charAt(0), '0'));
             }else newValue.insert(0, '0');
         }
-        System.out.println("Value: " + value);
-        System.out.println("newValue: " + newValue);
+//        System.out.println("Value: " + value);
+//        System.out.println("newValue: " + newValue);
 
         if(!(value.charAt(0) == '0')) {
             if ((Character.getNumericValue(value.charAt(value.length() - 1))) % 2 == 0) {
@@ -128,7 +129,108 @@ public class Intpossible {
     }
 
     public String toString() {
-        return "test";
+        return binaryReader(this.digits);
+    }
+
+//    private String binaryToDecimal(BooleanStack b){
+//        int decimal = 0;
+//        StringBuilder binary = new StringBuilder(b.toString());
+//        int power = 0;
+//        while(binary.length() != 0){
+//            int lastDigit = Character.getNumericValue(binary.charAt(binary.length() - 1));
+//            decimal += lastDigit*Math.pow(2, power);
+//            power++;
+//            binary.deleteCharAt(binary.length() - 1);
+//        }
+//        return "test";
+//
+//    }
+
+    private String binaryReader(BooleanStack b){
+        System.out.println("binary reader process started...");
+        boolean current = false;
+        StringBuilder value = new StringBuilder(b.getSize()/2);
+        value.append('0');
+        while(!(b.getSize() == 1)){
+            System.out.println("                CURRENT VALUE: " + value);
+            if(b.pop()){
+                value = addOne(value);
+                value = doubler(value);
+            }else{
+                value = doubler(value);
+            }
+        }
+        if(b.pop()){
+            value = addOne(value);
+        }
+        return value.toString();
+    }
+
+    private StringBuilder doubler(StringBuilder s){
+        System.out.println("doubler process started...");
+        int charValue;
+        Boolean carry = false;
+//
+//        for(int i = 0; i < s.length(); i++){
+//            int j = i;
+//            charValue = Character.getNumericValue(s.charAt(i));
+//            charValue += charValue;
+//
+//        }
+        for(int i = s.length() - 1; i >= 0; i--) {
+            charValue = Character.getNumericValue(s.charAt(i));
+            System.out.println("preDoubled: " + charValue);
+            charValue += charValue;
+            if(carry){
+                charValue += 1;
+                carry = false;
+            }
+            System.out.println("postDoubled: " + charValue);
+            if(charValue > 9){
+                carry = true;
+                charValue -= 10;
+                s.setCharAt(i, Character.forDigit(charValue, 10));
+                System.out.println("CHECK:" + s);
+                if(i > 0) {
+//                    s = addOne(s, i); //this may be where the method breaks
+                }else{
+                    s.insert(0, '1');
+                    return s;
+                }
+            }else{
+                s.setCharAt(i, Character.forDigit(charValue, 10));
+            }
+            System.out.println("VALUE (postDoubler) " + s);
+        }return s;
+    }
+
+    private StringBuilder addOne(StringBuilder s){
+        System.out.println("add one process started...");
+        int charValue = Character.getNumericValue(s.charAt(s.length() - 1));
+        System.out.println("preAdd: " + charValue);
+        charValue += 1;
+        System.out.println("postAdd: " + charValue);
+        if(charValue > 9){
+            charValue -= 10;
+            s = addOne(s, s.length() - 2);
+        }
+        s.setCharAt(s.length() - 1, Character.forDigit(charValue, 10));
+        System.out.println("VALUE (postAddOne) " + s);
+        return s;
+    }
+
+    private StringBuilder addOne(StringBuilder s, int index){
+        System.out.println("cary process started...");
+        System.out.println("preCarry: " + s);
+        int charValue = Character.getNumericValue(s.charAt(index));
+        charValue += 1;
+        if(charValue > 9){
+            charValue -= 10;
+            s = addOne(s, index - 1); //currently inspecting
+        }
+        s.setCharAt(index, Character.forDigit(charValue, 10));
+        System.out.println("VALUE (postCarry): " + s);
+        return s;
     }
 
 
@@ -144,6 +246,7 @@ public class Intpossible {
 
         private boolean[] stackArr;
         private int current = 0;
+        private int max = 0;
 
         protected BooleanStack(){
             stackArr = new boolean[10];
@@ -153,11 +256,13 @@ public class Intpossible {
             stackArr = new boolean[size];
         }
 
+
         protected void add(boolean value){
             if(current == stackArr.length - 1){
                 grow();
             }
             current++;
+            max++;
             stackArr[current] = value;
         }
 
@@ -189,6 +294,19 @@ public class Intpossible {
             return this.current;
         }
 
+        protected void reset(){
+            current = max;
+        }
+
+        protected void clear(){
+            current = 0;
+            max = 0;
+        }
+
+
+        public boolean[] getStackArr(){
+            return this.stackArr;
+        }
 
         @Override
         public String toString() {
